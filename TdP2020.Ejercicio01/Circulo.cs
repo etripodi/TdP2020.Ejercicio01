ea17652ad554a831cd31b5ee9163ff097fd662e1
Esteban Eduardo Tripodi Gauthier <etripodi@gmail.com>
2020-09-11 19:37:11 -0300

    Add project files.

---
+++ TdP2020.Ejercicio01/Circulo.cs
@@ -0,0 +1,50 @@
+﻿namespace TdP2020.Ejercicio01
+{
+    /// <summary>
+    /// Representa un círculo.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// Circulo circulo = new Circulo(1.1, 2.1, 5d);
+    /// </code>
+    /// </example>
+    class Circulo
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Punto iCentro;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private double iRadio;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pCentro"></param>
+        /// <param name="pRadio"></param>
+        public Circulo(Punto pCentro, double pRadio)
+        {
+        }
+
+        public Circulo(double pX, double pY, double pRadio) : this(new Punto(pX, pY), pRadio)
+        {
+            string
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Punto Centro { get { return this.iCentro; } }
+
+        public double Radio { get { return this.iRadio; } }
+
+        public double Area { get; }
+
+        public double Perimetro { get; }
+
+    }
+}
---
+++ TdP2020.Ejercicio01/Punto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TdP2020.Ejercicio01
+{
+    /// <summary>
+    /// Representa un punto.
+    /// </summary>
+    class Punto
+    {
+
+        /// <summary>
+        /// Constructor del punto.
+        /// </summary>
+        /// <param name="pX">Coordenada X.</param>
+        /// <param name="pY">Coordenada Y.</param>
+        public Punto(double pX, double pY)
+        {
+
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Calcula la distancia.
+        /// </summary>
+        /// <param name="pPunto">Punto con el cual se quiere determinar la distancia.</param>
+        /// <returns>Devuelve la distancia entre el punto y <paramref name="pPunto"/>.</returns>
+        public double CalcularDistanciaDesde(Punto pPunto)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
